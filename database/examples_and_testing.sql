81b546c92fed57d07892f40eb4ad7509f60d0fca
Ty Garber <tyler.garber@dfw.wa.gov>
2023-05-04 14:46:20 -0700

    r markdown examples

--- database/examples_and_testing.sql
+++ database/examples_and_testing.sql
@@ -73,9 +73,24 @@ union all
 select cal.catch_area_description
     parent_catch_area,
     cal2.catch_area_description children_catch_areas
+from catch_area_lut cal
+join catch_area_lut cal2 on cal.catch_area_id = cal2.parent_catch_area_id
+) select * from catch_areas;
+
+
+with recursive bag_limits as (
+select cal.catch_area_description as parent_catch_area,
+       null as children_catch_areas
+from catch_area_lut cal
+where parent_catch_area_id is null
+union all
+select cal.catch_area_description
+    parent_catch_area,
+    cal2.catch_area_description children_catch_areas
                                from catch_area_lut cal
                                         join catch_area_lut cal2 on cal.catch_area_id = cal2.parent_catch_area_id
 ) select * from catch_areas;
+
 
 /*
  insert some data
@@ -98,7 +113,7 @@ with fish as (
                                     start_datetime, end_datetime,
                                     gear_type_id, created_by, created_datetime)
         values ('691cab30-dc51-4835-98e4-ed4ab19c6244', 'd1aa8518-ffcf-4e70-a7b9-32874f916681',
-                '2022-01-01'::timestamp at time zone 'US/Pacific', '2022-12-31 23:59'::timestamp at time zone 'US/Pacific',
+                '2022-01-01'::timestamp at time zone 'American/Anchorage', '2022-12-31 23:59'::timestamp at time zone 'American/Anchorage',
                 '73ade5d3-180a-4136-b430-f593a2853c91', 'garber', now())
 )
 
@@ -106,11 +121,9 @@ with fish as (
 -- load some species groups into the species group table
 insert into species_group(species_id,  species_group_type_id)
 values
-    ('bad26e7e-ec39-478d-92b5-eb13b0032603', 'b455cd3f-f578-4fae-af90-2238bd225d75'),
-    ('b4416acc-2dce-4a14-a518-931eb45edbb1', 'b455cd3f-f578-4fae-af90-2238bd225d75'),
-    ('18ee22d5-8eed-46cf-9193-a5c767f1c965', 'b455cd3f-f578-4fae-af90-2238bd225d75'),
-    ('418ec7c4-1743-405c-86f6-e2307fe4615d', 'b455cd3f-f578-4fae-af90-2238bd225d75'),
-    ('a6d2c39b-281e-4817-ab4c-b50f93c89504', 'b455cd3f-f578-4fae-af90-2238bd225d75');
+    ('18ee22d5-8eed-46cf-9193-a5c767f1c965', 'b753e888-a3a2-4882-839e-ab2f9a6e533d'),
+    ('418ec7c4-1743-405c-86f6-e2307fe4615d', 'b753e888-a3a2-4882-839e-ab2f9a6e533d'),
+    ('a6d2c39b-281e-4817-ab4c-b50f93c89504', 'b753e888-a3a2-4882-839e-ab2f9a6e533d');
 
 -- parent bag limit (daily)
 insert into bag_limit(
@@ -142,41 +155,25 @@ values (
 insert into fishery(fishery_type_id, fishery_management_year_id, catch_area_id, fishery_description,
                     regulation_authority_id, created_by, created_datetime)
 values
-    ('ff2076b7-7f01-4938-bb77-73a8f16d8c84', '40b5fe35-e081-482e-a4ee-ac053537a425', 'a4cc93a0-bf8a-4564-b482-3c8b415f1550', 'Yakutat',
+    ('ff2076b7-7f01-4938-bb77-73a8f16d8c84', '40b5fe35-e081-482e-a4ee-ac053537a425', '78abc06a-12d5-424c-8b7e-4932aa9605cf', 'outside of Yakutat',
         '7a325360-faab-46ba-bfe9-19d7cd6991b6', 'garber', now())
+returning fishery_id;
 
 -- insert some times/dates associated with those fisheries
-with fish as (
-    insert into fishery_regulation (fishery_id, fishery_regulation_type_id,
-                                    start_datetime, end_datetime,
-                                    gear_type_id, created_by, created_datetime)
-        values ('691cab30-dc51-4835-98e4-ed4ab19c6244', 'd1aa8518-ffcf-4e70-a7b9-32874f916681',
-                '2022-08-16'::timestamp at time zone 'US/Pacific', '2022-08-20 23:59'::timestamp at time zone 'US/Pacific',
-                '73ade5d3-180a-4136-b430-f593a2853c91', 'garber', now()),
-               ('dc820f06-8451-4830-b9c0-6e59574323d7', 'd1aa8518-ffcf-4e70-a7b9-32874f916681',
-                '2022-08-22'::timestamp at time zone 'US/Pacific', '2022-08-27 23:59'::timestamp at time zone 'US/Pacific',
-                '73ade5d3-180a-4136-b430-f593a2853c91', 'garber', now()),
-                ('dc820f06-8451-4830-b9c0-6e59574323d7', 'd1aa8518-ffcf-4e70-a7b9-32874f916681',
-                '2022-08-29'::timestamp at time zone 'US/Pacific', '2022-09-03 23:59'::timestamp at time zone 'US/Pacific',
-                '73ade5d3-180a-4136-b430-f593a2853c91', 'garber', now()),
-                ('dc820f06-8451-4830-b9c0-6e59574323d7', 'd1aa8518-ffcf-4e70-a7b9-32874f916681',
-                '2022-09-07'::timestamp at time zone 'US/Pacific', '2022-09-10 23:59'::timestamp at time zone 'US/Pacific',
-                '73ade5d3-180a-4136-b430-f593a2853c91', 'garber', now()),
-                ('dc820f06-8451-4830-b9c0-6e59574323d7', 'd1aa8518-ffcf-4e70-a7b9-32874f916681',
-                '2022-09-14'::timestamp at time zone 'US/Pacific', '2022-09-17 23:59'::timestamp at time zone 'US/Pacific',
-                '73ade5d3-180a-4136-b430-f593a2853c91', 'garber', now()),
-                ('dc820f06-8451-4830-b9c0-6e59574323d7', 'd1aa8518-ffcf-4e70-a7b9-32874f916681',
-                '2022-09-21'::timestamp at time zone 'US/Pacific', '2022-09-24 23:59'::timestamp at time zone 'US/Pacific',
-                '73ade5d3-180a-4136-b430-f593a2853c91', 'garber', now()),
-                ('dc820f06-8451-4830-b9c0-6e59574323d7', 'd1aa8518-ffcf-4e70-a7b9-32874f916681',
-                '2022-09-28'::timestamp at time zone 'US/Pacific', '2022-09-30 23:59'::timestamp at time zone 'US/Pacific',
-                '73ade5d3-180a-4136-b430-f593a2853c91', 'garber', now())
-)
+insert into fishery_regulation (fishery_id, fishery_regulation_type_id,
+                                start_datetime, end_datetime,
+                                gear_type_id, created_by, created_datetime)
+    values ('c79a8e8b-8f99-4e8a-b48e-cfb09b0f7074', 'd1aa8518-ffcf-4e70-a7b9-32874f916681',
+            '2023-01-01'::timestamp at time zone 'America/Anchorage', '2023-12-31 23:59'::timestamp at time zone 'America/Anchorage',
+            '73ade5d3-180a-4136-b430-f593a2853c91', 'garber', now())
+returning fishery_regulation_id;
 
-update fishery_regulation
-    set start_datetime = '2022-01-01'::timestamp at time zone 'America/Anchorage',
-        end_datetime = '2022-12-31 23:59'::timestamp at time zone 'America/Anchorage'
-where fishery_regulation_id = '3932dc50-b870-46c1-b568-116a893afbe6'
+
+show timezone
+-- update fishery_regulation
+--     set start_datetime = '2022-01-01'::timestamp at time zone 'America/Anchorage',
+--         end_datetime = '2022-12-31 23:59'::timestamp at time zone 'America/Anchorage'
+-- where fishery_regulation_id = '3932dc50-b870-46c1-b568-116a893afbe6'
 
 
 -- select * from fishery_regulation
@@ -191,30 +188,34 @@ values
     ('a6d2c39b-281e-4817-ab4c-b50f93c89504', 'b455cd3f-f578-4fae-af90-2238bd225d75');
 
 -- parent bag limit (daily)
-insert into bag_limit(
-                       fishery_regulation_id, regulation_age_id, species_group_type_id,
-                       regulation_type_id, bag_limit_type_id, maximum_size_limit_centimeters,
-                      minimum_size_limit_centimeters, bag_limit_total, created_by, created_datetime)
-values (
-        '4341be73-00db-4e94-b7ed-3383a360465c', '5c29ce48-ff99-41e3-9381-93b2564df906', 'b455cd3f-f578-4fae-af90-2238bd225d75',
-        '4556aaa7-6f1d-4e73-ae29-cc6780098dcd', '99116fee-7807-4473-9912-575fa52263e9', NULL, 12 * 2.54, 6,
-        'garber', now()
-       );
+-- insert into bag_limit(
+--                        fishery_regulation_id, regulation_age_id, species_group_type_id,
+--                        regulation_type_id, bag_limit_type_id, maximum_size_limit_centimeters,
+--                       minimum_size_limit_centimeters, bag_limit_total, created_by, created_datetime)
+-- values (
+--         '4341be73-00db-4e94-b7ed-3383a360465c', '5c29ce48-ff99-41e3-9381-93b2564df906', 'b455cd3f-f578-4fae-af90-2238bd225d75',
+--         '4556aaa7-6f1d-4e73-ae29-cc6780098dcd', '99116fee-7807-4473-9912-575fa52263e9', NULL, 12 * 2.54, 6,
+--         'garber', now()
+--        );
 
 -- child bag limit
 insert into bag_limit( -- this will need resident status
-                       parent_bag_limit_id, fishery_regulation_id, regulation_age_id, species_group_type_id,
-                       regulation_type_id, bag_limit_type_id, maximum_size_limit_centimeters,
-                      minimum_size_limit_centimeters, bag_limit_total, created_by, created_datetime)
+                    parent_bag_limit_id,
+                      fishery_regulation_id, regulation_age_id, species_group_type_id,
+                      regulation_type_id, bag_limit_type_id, maximum_size_limit_centimeters,
+                      minimum_size_limit_centimeters, bag_limit_total, created_by, created_datetime, bag_limit_angler_resident_status_id)
 values (
-        '0373c9af-025e-42fc-95cc-058c5a80c75e','4341be73-00db-4e94-b7ed-3383a360465c', '850a2137-43ab-4327-8f49-3b50ca8b6dbc', '216ee679-e466-41ca-8cb4-590cc3f7709e',
-        'ab43a5b2-34a0-41ee-8063-9250162cad67', 'cb489ec8-c153-4b86-a395-bc1169390f9b', NULL, 12 * 2.54, 2,
-        'garber', now()
-       );
-
+        '159ec896-1bca-4404-bd5e-8291bd92efe6',
+        '339b5ad1-0bea-4276-938d-5fd34bf4effa', '5c29ce48-ff99-41e3-9381-93b2564df906', '3996180c-2b76-4fb1-9f5c-2b8a8feef529',
+        'ab43a5b2-34a0-41ee-8063-9250162cad67', '99116fee-7807-4473-9912-575fa52263e9', NULL , 16 *2.54, 6,
+        'garber', now(), '833b63d1-7060-44de-bed5-1949f563dcdc'
+       ) returning bag_limit_id;
 -- query to get stuff out
 set timezone = 'America/Anchorage'
 select
+    bag_limit_id,
+    fr.fishery_regulation_id,
+    f.fishery_id,
     f.fishery_description,
     fr.start_datetime,
     fr.end_datetime,
@@ -225,15 +226,19 @@ select
     ra.regulation_age_description,
     bl.minimum_size_limit_centimeters,
     bl.maximum_size_limit_centimeters,
-    bltl.big_limit_type_description,
+    bltl.bag_limit_type_description,
     blarsl.bag_limit_angler_resident_status_description,
-    bl.bag_limit_total
+    bl.bag_limit_total,
+    bl.created_datetime
 from
+
     fishery f
 join fishery_regulation fr on f.fishery_id = fr.fishery_id
 join bag_limit bl on fr.fishery_regulation_id = bl.fishery_regulation_id
 join catch_area_lut cal on f.catch_area_id = cal.catch_area_id
 join species_group_type_lut sl on bl.species_group_type_id = sl.species_group_type_id
+--join species_group sg on sl.species_group_type_id = sg.species_group_type_id
+--join species_lut sls on sg.species_id = sls.species_id
 join regulation_authority_lut ral on f.regulation_authority_id = ral.regulation_authority_id
 join regulation_type_lut rtl on bl.regulation_type_id = rtl.regulation_type_id
 join regulation_age_lut ra on bl.regulation_age_id = ra.regulation_age_id
@@ -241,3 +246,12 @@ join bag_limit_type_lut bltl on bl.bag_limit_type_id = bltl.bag_limit_type_id
 join bag_limit_angler_resident_status_lut blarsl on bl.bag_limit_angler_resident_status_id = blarsl.bag_limit_angler_resident_status_id
 where
     regulation_authority_code= 'ADFG'
+order by species_group_type_description;
+
+
+create user ro_user with password 'salmon'
+
+alter group read_only add user ro_user;
+
+grant connect on database postgres to ro;
+grant usage on schema public to ro;
