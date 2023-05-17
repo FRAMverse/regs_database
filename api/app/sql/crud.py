86d3164ab29392c644d95ffb367d3fa60e362a4a
Ty Garber <tyler.garber@dfw.wa.gov>
2023-05-17 14:16:54 -0700

    finished off the meta router orientating myself with some sqlalchemy queries

--- api/app/sql/crud.py
+++ api/app/sql/crud.py
@@ -1,20 +1,36 @@
-from sqlalchemy.orm import Session, joinedload
+from sqlalchemy.orm import Session, selectinload
 from sqlalchemy import func
 from uuid import UUID
 from sqlalchemy.dialects import postgresql
 from zoneinfo import ZoneInfo
 from . import models, schemas
+from fastapi import HTTPException
+
 
 
 def get_catch_areas(db: Session):
-    return db.query(models.CatchAreaLUT).options(joinedload(models.CatchAreaLUT.childen_catch_areas)).filter(models.CatchAreaLUT.parent_catch_area_id == None).all()
+    return db.query(models.CatchAreaLUT).options(selectinload(models.CatchAreaLUT.childen_catch_areas)).filter(models.CatchAreaLUT.parent_catch_area_id == None).all()
 
-def lut(db: Session, lut: str):
+def lut(db: Session, requested_table: str):
+    # allowed tables, only look up tables should be hitting this
     allowed_tables = {
         'catch_area' : models.CatchAreaLUT,
-        'regulation_type': models.RegulationTypeLUT
+        'regulation_type': models.RegulationTypeLUT,
+        'regulation_age': models.RegulationAgeLUT,
+        'regulation_autority': models.RegulationAuhtorityLUT,
+        'bag_limit_type': models.BagLimitTypeLUT,
+        'resident_status': models.BagLimitResidentStatusLUT,
+        'gear_type': models.GearTypeLUT,
+        'management_year': models.FisheryManagementYearLUT,
+        'fishery_regulation_type': models.FisheryRegulationTypeLUT,
+        'fishery_type': models.FisheryTypeLUT,
+        'species': models.SpeciesLUT
     }
-    return db.query(allowed_tables[lut]).all()
+    # check if requested table is available, if not throw a  404
+    if requested_table not in allowed_tables:
+        raise HTTPException(status_code = 404, detail='Table not found')
+    else:
+        return db.query(allowed_tables[requested_table]).all()
 
 
 
@@ -22,8 +38,14 @@ def get_regs(db: Session):
     return db.query(models.Fishery, 
                     models.CatchAreaLUT.catch_area_id, \
                     models.CatchAreaLUT.catch_area_description) \
-                    .options(joinedload(models.Fishery.fishery_regulation)) \
-                    .join(models.CatchAreaLUT).all()
+                    .join(models.CatchAreaLUT) \
+                    .options(
+                        selectinload(
+                            models.Fishery.fishery_regulation
+                            )\
+                            .selectinload(models.FisheryRegulation.gear_type)
+                            ) \
+                    .all()
 # def get_geo(db: Session, catch_area_id: UUID):
 #     return db.query(models.CatchArea).filter(models.CatchArea.catch_area_id == catch_area_id).first()
 
